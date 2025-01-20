54837994a508cee03e9ca45c56b1663ab17fa27a
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-01-20 12:14:03 +0200

    Video 2 - Stack

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,12 +5,30 @@
         static void Main(string[] args)
         {
             #region Generic Collection - LinkedList
-            LinkedList<int> Numbers = new LinkedList<int>();
-            Numbers.AddFirst(1);
-            Numbers.AddAfter(Numbers.First, 2);
-            Numbers.AddAfter(Numbers.First.Next, 3);
-            Numbers.AddLast(4);
-            foreach(int number in Numbers)
+            //LinkedList<int> Numbers = new LinkedList<int>();
+            //Numbers.AddFirst(1);
+            //Numbers.AddAfter(Numbers.First, 2);
+            //Numbers.AddAfter(Numbers.First.Next, 3);
+            //Numbers.AddLast(4);
+            //foreach(int number in Numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            #endregion
+
+            #region Stack
+            Stack<int> Numbers = new Stack<int>();
+            Numbers.Push(1);
+            Numbers.Push(2);
+            Numbers.Push(3);
+            foreach (int number in Numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Numbers.Pop();
+            Console.WriteLine( Numbers.TryPop(out int result));
+            Console.WriteLine("After Pop");
+            foreach (int number in Numbers)
             {
                 Console.WriteLine(number);
             }
