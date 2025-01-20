4d82d6b324ed72c51fb80eff7523bf3e74b84a51
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-01-20 14:55:15 +0200

    Video 6 - SortedDictionary

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -65,12 +65,23 @@ namespace Demo
             #endregion
 
             #region Dictionary (HashTable)
-            Dictionary<string, int> Note = new Dictionary<string, int>();
-            Note.Add("Ahmed", 123);
+            //Dictionary<string, int> Note = new Dictionary<string, int>();
+            //Note.Add("Ahmed", 123);
+            //Note.Add("Mostafa", 456);
+            //Note.Add("omar", 789);  
+            //Note.Add("Ahmed", 038);
+            //foreach (KeyValuePair<string, int> item in Note)
+            //{
+            //    Console.WriteLine($"{item.Key} :: {item.Value}");
+            //}
+            #endregion
+
+            #region SortedDictionary (BST)
+            SortedDictionary<string, int> Note = new SortedDictionary<string, int>();
             Note.Add("Mostafa", 456);
-            Note.Add("omar", 789);  
-            Note.Add("Ahmed", 038);
-            foreach (KeyValuePair<string, int> item in Note)
+            Note.Add("omar", 789);
+            Note.Add("Ahmed", 123);
+            foreach (var item in Note)
             {
                 Console.WriteLine($"{item.Key} :: {item.Value}");
             }
